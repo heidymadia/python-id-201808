5b553fc3c38c1f101027ca0db2f9a66a3d643637
Nicholas Hamidjaja <nicholas.hamidjaja@shopee.com>
2018-08-06 14:02:14 +0700

    Implement mod with non zero remainder

--- noob.py
+++ noob.py
@@ -2,5 +2,9 @@ def mod(a, b):
     if b == 0:
         raise ZeroDivisionError
     
-    return 0
+    remainder = a
+    while remainder >= b:
+        remainder -= b
+    
+    return remainder
     
--- noob_test.py
+++ noob_test.py
@@ -7,7 +7,10 @@ class TestNoob(unittest.TestCase):
         self.assertRaises(ZeroDivisionError, noob.mod, 1 , 0)
 
     def test_mod_zero_remainder(self):
-        self.assertEqual(0, noob.mod(15,3))
+        self.assertEqual(0, noob.mod(15, 3))
+
+    def test_mod_non_zero_remainder(self):
+        self.assertEqual(3, noob.mod(24, 7))
  
 if __name__ == '__main__':
     unittest.main()
