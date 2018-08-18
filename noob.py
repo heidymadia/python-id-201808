b12808f8b259f81f05380e2fabdaf5993272a1e8
Nicholas Hamidjaja <nicholas.hamidjaja@shopee.com>
2018-08-18 16:32:11 +0700

    Refactor mod using division and multiplication

--- noob.py
+++ noob.py
@@ -1,10 +1,5 @@
 def mod(a, b):
-    if b == 0:
-        raise ZeroDivisionError
-    
-    remainder = a
-    while remainder >= b:
-        remainder -= b
-    
-    return remainder
+    quotient = int(a / b)
+
+    return (a - quotient * b)
     
--- noob_test.py
+++ noob_test.py
@@ -4,7 +4,7 @@ import noob
 class TestNoob(unittest.TestCase):
  
     def test_mod_divide_by_zero(self):
-        self.assertRaises(ZeroDivisionError, noob.mod, 1 , 0)
+        self.assertRaises(ZeroDivisionError, noob.mod, 1 ,0)
 
     def test_mod_zero_remainder(self):
         self.assertEqual(0, noob.mod(15, 3))
